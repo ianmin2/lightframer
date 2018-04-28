f9545ecf881d50c6719141e1a31e4caa418e48e4
Ian Innocent mbae <ianmin2@live.com>
2018-04-28 08:05:54 +0300

    Optimization and bloat removal on UI module

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -3,35 +3,35 @@ var concat      = require('gulp-concat');
 var rename      = require('gulp-rename');  
 var uglify      = require('gulp-uglify'); 
 
-var babel = require("gulp-babel");
-var plumber = require("gulp-plumber");
+var babel       = require("gulp-babel");
+var plumber     = require("gulp-plumber");
  
-var paths = {
-  es6: ['./src/*.es6']
-  ,framework: ['./src/framework/framify.es6']
+var paths       = {
+    es6: ['./src/*.es6']
+    ,framework: ['./src/framework/framify.es6']
 };
  
 gulp.task('default', ['es6','framify']);
  
-gulp.task("es6", function () {
-  return gulp.src(paths.es6)
+gulp.task("es6",  () =>
+     gulp.src(paths.es6)
     .pipe(plumber())
     .pipe(babel({presets: ['es2015']}))
-    .pipe(gulp.dest("assets/js"));
-});
+    .pipe(gulp.dest("assets/js"))
+);
  
 //...
  
-gulp.task('watch', function() {
-  gulp.watch(paths.es6, ['es6','framify']);
-});
+gulp.task('watch', () =>
+    gulp.watch(paths.es6, ['es6','framify'])
+);
 
-gulp.task('framify', function() {  
-    return gulp.src(paths.framework)
-        .pipe(babel({ presets: ['es2015'] }))
-        .pipe(concat('framify.js'))
-        .pipe(gulp.dest('assets/js'))
-        .pipe(rename('framify.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('assets/js'));
-});
+gulp.task('framify', () =>
+    gulp.src(paths.framework)
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(concat('framify.js'))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(rename('framify.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'))
+);
