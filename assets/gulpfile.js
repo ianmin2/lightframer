2dbb765bd4dc8ebf1484335cd0f5a129cfedf007
Ian Innocent mbae <ianmin2@live.com>
2018-04-29 06:08:33 +0300

    Bloatware Fix for UI

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,37 +1,45 @@
-var gulp        = require('gulp');
-var concat      = require('gulp-concat');  
-var rename      = require('gulp-rename');  
-var uglify      = require('gulp-uglify'); 
+const gulp        = require('gulp');
+const concat      = require('gulp-concat');  
+const rename      = require('gulp-rename');  
+const uglify      = require('gulp-uglify'); 
 
-var babel       = require("gulp-babel");
-var plumber     = require("gulp-plumber");
+const babel = require("gulp-babel");
+const plumber = require("gulp-plumber");
  
-var paths       = {
-    es6: ['./src/*.es6']
-    ,framework: ['./src/framework/framify.es6']
+// const linter      = require("gulp-jshint");
+
+const paths = {
+  es6: ['./src/*.es6']
+  ,framework: ['./src/framework/framify.es6']
 };
  
 gulp.task('default', ['es6','framify']);
  
-gulp.task("es6",  () =>
-     gulp.src(paths.es6)
+gulp.task("es6", function () {
+  return gulp.src(paths.es6)
     .pipe(plumber())
     .pipe(babel({presets: ['es2015']}))
-    .pipe(gulp.dest("assets/js"))
-);
+    .pipe(gulp.dest("assets/js"));
+});
  
 //...
  
-gulp.task('watch', () =>
-    gulp.watch(paths.es6, ['es6','framify'])
-);
+gulp.task('watch', function() {
+  gulp.watch(paths.es6, ['es6','framify']);
+});
 
-gulp.task('framify', () =>
-    gulp.src(paths.framework)
-    .pipe(babel({ presets: ['es2015'] }))
-    .pipe(concat('framify.js'))
-    .pipe(gulp.dest('assets/js'))
-    .pipe(rename('framify.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('assets/js'))
-);
+gulp.task('framify', function() {  
+    return gulp.src(paths.framework)
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(concat('framify.js'))
+        .pipe(gulp.dest('assets/js'))
+        .pipe(rename('framify.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js'));
+});
+
+// gulp.task('lint',()=>
+//   gulp.src(  paths.framework.concat(paths.es6) )
+//   .pipe(linter())
+//   .pipe(linter.reporter())
+// );
